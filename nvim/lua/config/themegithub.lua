46b3695fcc00e16eec2ebc5de3054c2e324a2736
gabriel torres <gabriel.torres@znc.com.br>
2023-06-08 14:47:46 -0300

    Atualizado config do github-theme

--- nvim/lua/config/themegithub.lua
+++ nvim/lua/config/themegithub.lua
@@ -1,21 +1,22 @@
 require("github-theme").setup({
-    theme_style = "dark_colorblind",
-    transparent = true,
-    function_style = "italic",
-    keyword_style = "italic",
-    variable_style = "italic",
-    sidebars = { "qf", "vista_kind", "terminal", "packer" },
+    options = {
+        transparent = false,
+        style = {
+            theme = "dark_colorblind",
+            functions = "italic",
+            keyword = "italic",
+            variable = "italic",
+        },
+        darken = {
+            sidebars = {
+                enable = true,
+                list = { "qf", "vista_kind", "terminal", "packer" },
+            },
+        },
+    },
 
     -- Change the "hint" color to the "orange" color, and make the "error" color bright red
-    colors = { hint = "orange", error = "#ff0000" },
-
-    -- Overwrite the highlight groups
-    overrides = function(c)
-        return {
-            htmlTag = { fg = c.bright, sp = c.red },
-            DiagnosticHint = { link = "LspDiagnosticsDefaultHint" },
-            -- this will remove the highlight groups
-            TSField = {},
-        }
-    end
+    palettes = { hint = "orange", error = "#ff0000" },
 })
+
+vim.cmd('colorscheme github_dark_high_contrast')
